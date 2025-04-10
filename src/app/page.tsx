5a07363b1e6a9d0fb4563543f14a89259403cfca
Gabriel-otirB <lordglayder@gmail.com>
2025-04-10 11:42:20 -0300

    feature: Creating dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image';
+import heroImg from '@/assets/hero.svg';
+
 export default function Home() {
   return (
-    <main>
-      <h1>TESTE</h1>
+    <main className='flex items-center flex-col justify-center min-h-[calc(100vh-80px)]'>
+      <h2 className='font-medium text-2xl mb-2'>Gerencie sua empresa</h2>
+      <h1 className='font-bold text-3xl mb-8 text-purple-900 md:text-4xl'>Atendimentos, clientes</h1>
+      <Image
+        src={heroImg}
+        alt="Image hero do Gestor One"
+        width={600}
+        height={600}
+        className='max-w-sm md:max-w-xl'
+      />
     </main>
   );
 }
