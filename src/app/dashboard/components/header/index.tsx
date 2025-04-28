ea4936af925372e1fd9ed0153ff016c8bdf4ee2b
Gabriel-otirB <lordglayder@gmail.com>
2025-04-28 19:28:15 -0300

    feature: Creating dashboard

---
+++ src/app/dashboard/components/header/index.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Container from '@/components/container';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const DashboardHeader = () => {
+  const pathname = usePathname();
+
+  const isActive = (path: string) => pathname === path;
+
+  return (
+    <Container>
+      <header className="w-full bg-gray-900 my-4 p-3 rounded flex gap-4">
+        <Link
+          href="/dashboard"
+          className={`text-white transition-all duration-300 flex items-center hover:scale-[1.025] ${
+            isActive('/dashboard') ? 'font-bold underline underline-offset-4' : ''
+          }`}
+        >
+          Chamados
+        </Link>
+        <Link
+          href="/dashboard/customer"
+          className={`text-white transition-all duration-300 flex items-center hover:scale-[1.025] ${
+            isActive('/dashboard/customer') ? 'font-bold underline underline-offset-4' : ''
+          }`}
+        >
+          Clientes
+        </Link>
+      </header>
+    </Container>
+  );
+};
+
+export default DashboardHeader;
