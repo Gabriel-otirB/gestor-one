2cf95a9f8a5f7c3411da8db6b8eb841885c87049
Gabriel-otirB <lordglayder@gmail.com>
2025-05-23 12:37:05 -0300

    feature: Deleting customers

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -3,6 +3,43 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import prismaClient from '@/lib/prisma';
 
+export async function DELETE(request: Request) {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return NextResponse.json({ message: "User not authenticated." }, { status: 401 });
+  }
+
+  const { searchParams } = new URL(request.url);
+  const userId = searchParams.get("id");
+
+  if (!userId) {
+    return NextResponse.json({ message: "Customer not found." }, { status: 400 });
+  }
+
+  const findTickets = await prismaClient.ticket.findFirst({
+    where: {
+      customerId: userId as string
+    }
+  })
+
+  if (findTickets) {
+    return NextResponse.json({ message: "Customer has tickets." }, { status: 400 });
+  }
+
+  try {
+    await prismaClient.customer.delete({
+      where: {
+        id: userId as string
+      }
+    })
+
+    return NextResponse.json({ message: "Customer deleted successfully!" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Failed to delete customer." }, { status: 400 });
+  }
+}
+
 export async function POST(request: Request) {
   const session = await getServerSession(authOptions);
 
@@ -28,6 +65,4 @@ export async function POST(request: Request) {
   } catch (error) {
     return NextResponse.json({ message: "Failed to create customer." }, { status: 400 });
   }
-
-  return NextResponse.json({ message: "ok" });
 }
--- src/app/dashboard/customer/components/card/index.tsx
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -1,6 +1,37 @@
+"use client";
+
+import { api } from '@/lib/api';
 import { CustomerProps } from '@/utils/customer.type';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export function CardCustomer({ customer }: { customer: CustomerProps }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const handleDeleteCustomer = async () => {
+    try {
+      setLoading(true);
+      const response = await api.delete("/api/customer", {
+        params: {
+          id: customer.id
+        }
+      });
+
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (loading) {
+    return (
+      <div className="fixed inset-0 flex items-center justify-center z-50 bg-white/70">
+        <div className="w-8 h-8 border-3 border-purple-700 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    )
+  }
+
   return (
     <article className='flex flex-col bg-gray-100 border-2 p-2 rounded-lg gap-2 hover:scale-[1.03] duration-300'>
       <h2>
@@ -9,7 +40,10 @@ export function CardCustomer({ customer }: { customer: CustomerProps }) {
       <p><a className='font-bold mr-1'>Email:</a>{customer.email}</p>
       <p><a className='font-bold mr-1'>Telefone:</a>{customer.phone}</p>
 
-      <button className='bg-red-500 px-4 rounded text-white mt-2 self-start cursor-pointer'>
+      <button
+        className='bg-red-500 px-4 rounded text-white mt-2 self-start cursor-pointer'
+        onClick={handleDeleteCustomer}
+      >
         Deletar
       </button>
     </article>
