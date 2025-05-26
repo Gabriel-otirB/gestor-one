fc8238d7929f1ae327e0038b341997e3927b078d
Gabriel-otirB <lordglayder@gmail.com>
2025-05-26 15:29:34 -0300

    Creating new ticket page

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -17,7 +17,7 @@ const Header = () => {
 
   return (
     <header className='w-full flex items-center px-2 py-4 bg-white h-20 shadow-sm'>
-      <div className='w-full flex items-center justify-between max-w-7xl mx-auto items-baseline'>
+      <div className='w-full flex items-center justify-between max-w-7xl mx-auto'>
 
         <Link href="/">
           <h1 className='font-bold text-2xl pl-1 hover:tracking-widest duration-300 cursor-pointer'>
